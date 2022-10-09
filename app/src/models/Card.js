409418be18b9bc0e029f062c51fa2431089aaeac
trsaints <thiago.dev2k3@gmail.com>
2022-10-09 16:14:48 -0300

    Criação de novos componentes

---
+++ app/src/models/Card.js
@@ -0,0 +1,58 @@
+import { elementController } from "../controllers/element-controller.js";
+
+export class Card {
+  #generateBanner = (product) => {
+    const fig = elementController.generate("figure", "card__figure");
+    const caption = elementController.generate(
+      "figcaption",
+      "card__figure--caption"
+    );
+    const img = elementController.generate("img", "card__figure--image");
+
+    const content = {
+      title: product.name,
+      image: product.image,
+    };
+
+    caption.textContent = content.title;
+    img.setAttribute("src", `./app/assets/images/${content.image}`);
+
+    elementController.render(caption, fig);
+    elementController.render(img, fig);
+
+    return fig;
+  };
+
+  #generateContent = (product) => {
+    const wrapper = elementController.generate("div", "card__content");
+    const desc = elementController.generate("p", "card__content--desc");
+    const price = elementController.generate("span", "card__content--price");
+
+    const content = {
+      desc: product.desc,
+      price: product.price,
+    };
+
+    price.textContent = content.price;
+    desc.textContent = content.desc;
+
+    elementController.render(desc, wrapper);
+    elementController.render(price, wrapper);
+
+    return wrapper;
+  };
+
+  #generate = (product) => {
+    const frag = document.createDocumentFragment();
+    const li = elementController.generate("li", "card");
+    elementController.render(this.#generateBanner(product), li);
+    elementController.render(this.#generateContent(product), li);
+    elementController.render(li, frag);
+
+    return frag;
+  };
+
+  constructor(product) {
+    return this.#generate(product);
+  }
+}
---
+++ app/src/models/ProductCatalog.js
@@ -0,0 +1,92 @@
+import { contextController } from "../controllers/context-controller.js";
+import { elementController } from "../controllers/element-controller.js";
+import { Card } from "./Card.js";
+
+export class ProductCatalog {
+  #renderCard = (product, target) => {
+    const card = new Card(product);
+    elementController.render(card, target);
+  };
+
+  #generateList = (products) => {
+    const frag = document.createDocumentFragment();
+    const ul = elementController.generate("ul", "products__list");
+
+    products.forEach((product) => {
+      this.#renderCard(product, ul);
+    });
+
+    elementController.render(ul, frag);
+
+    return frag;
+  };
+
+  #hideList = (list) => {};
+
+  #setOption = {
+    index: (button) => {
+      button.setAttribute("data-load", "products");
+      button.textContent = "Ver Todos";
+      button.addEventListener("click", () =>
+        contextController.render("products")
+      );
+    },
+    products: (button) => {
+      button.setAttribute("data-toggle", "list");
+      button.textContent = "Mostrar";
+      button.addEventListener("click", () =>
+        this.#hideList(button.parentNode.parentNode)
+      );
+    },
+  };
+
+  #generateOption = (context) => {
+    const option = elementController.generate(
+      "button",
+      "products__header--option"
+    );
+
+    this.#setOption[context](option);
+
+    return option;
+  };
+
+  #generateHeader = (context, category) => {
+    const frag = document.createDocumentFragment();
+    const header = elementController.generate("header", "products__header");
+    const title = elementController.generate("h2", "products__header--title");
+    const option = this.#generateOption(context);
+
+    const categories = {
+      canvases: "Quadros",
+      keyrings: "Chaveiros",
+      actionFigures: "Action Figures",
+      consoles: "Consoles",
+    };
+
+    title.textContent = categories[category];
+
+    elementController.render(title, header);
+    elementController.render(option, header);
+    elementController.render(header, frag);
+
+    return frag;
+  };
+
+  #generateCatalog = (context, products) => {
+    const frag = document.createDocumentFragment();
+    const catalog = elementController.generate("div", "main__products");
+    const header = this.#generateHeader(context, products.category);
+    const list = this.#generateList(products[products.category]);
+
+    elementController.render(header, catalog);
+    elementController.render(list, catalog);
+    elementController.render(catalog, frag);
+
+    return frag;
+  };
+
+  constructor(context, products) {
+    return this.#generateCatalog(context, products);
+  }
+}
