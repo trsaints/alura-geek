9074a903123d6e4897f400ff238206f1178b25b8
trsaints <thiago.dev2k3@gmail.com>
2022-10-09 16:10:58 -0300

    Alteração na execução do script

--- app/src/app.js
+++ app/src/app.js
@@ -1,27 +1,32 @@
 import { contextController } from "./controllers/context-controller.js";
+import { productsController } from "./controllers/products-controller.js";
+import { contextService } from "./services/context-service.js";
 import { databaseService } from "./services/database-service.js";
 
 (() => {
   const productBtn = document.querySelector('[data-load="products"]');
   const loginBtn = document.querySelector('[data-load="login"]');
   const searchBtn = document.querySelector('[data-load="search"]');
-
   const contextWrapper = document.querySelector("[data-context]");
 
   productBtn.addEventListener("click", () => {
-    contextController.render("products", contextWrapper);
+    contextController.render("products");
   });
 
   searchBtn.addEventListener("click", () => {
-    contextController.render("products", contextWrapper);
+    contextController.render("search");
   });
 
   loginBtn.addEventListener("click", () => {
-    contextController.render("login", contextWrapper);
     loginBtn.remove();
+    contextController.render("login");
   });
 
   if (!databaseService.checkPreLoad()) {
     databaseService.configure();
+  }
+
+  if (contextService.check(contextWrapper, "index")) {
+    productsController.renderCatalogs();
   }
 })();
