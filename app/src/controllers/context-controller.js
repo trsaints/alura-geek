645456fe62dbe747c2c58ef3ad390613af6a107d
trsaints <thiago.dev2k3@gmail.com>
2022-10-17 09:44:33 -0300

    MutationObserver implementado para renderizações

--- app/src/controllers/context-controller.js
+++ app/src/controllers/context-controller.js
@@ -1,5 +1,6 @@
 import { contextService } from "../services/context-service.js";
 import { elementController } from "./element-controller.js";
+import { productsController } from "./products-controller.js";
 
 let load = (page) => {
   const main = document.querySelector("main");
@@ -7,13 +8,11 @@ let load = (page) => {
   main.appendChild(page);
 };
 
-let loadPage = (page, target, context) => {
+let loadPage = (page, target) => {
   const parser = new DOMParser();
 
   const result = parser.parseFromString(page, "text/html");
-  const resultMain = result.querySelector("[data-load='context']");
-
-  document.body.setAttribute("data-context", context);
+  const resultMain = result.querySelector("[data-main='context']");
 
   target.appendChild(resultMain);
 };
@@ -40,11 +39,52 @@ let render = (context) => {
 
   contextService
     .fetch(context)
-    .then((page) => loadPage(page, frag, context))
+    .then((page) => loadPage(page, frag))
     .catch((error) => loadPlaceholder(error, frag))
     .finally(() => load(frag));
 };
 
+const renderFactory = {
+  products: () => {
+    render("products");
+
+    setTimeout(productsController.renderCatalogs, 500);
+  },
+
+  search: () => {
+    render("search");
+  },
+
+  login: () => {
+    render("login");
+  },
+};
+
+const observe = (target) => {
+  const config = {
+    attributes: true,
+    childList: false,
+    subtree: false,
+  };
+
+  const checkMutation = (mutationList, observer) => {
+    mutationList.forEach((mutation) => {
+      const hasChangedContext =
+        mutation.type === "attributes" &&
+        contextService.get(target) !== undefined;
+
+      if (hasChangedContext) {
+        const context = contextService.get(target);
+        renderFactory[context]();
+      }
+    });
+  };
+
+  const observer = new MutationObserver(checkMutation);
+
+  observer.observe(target, config);
+};
+
 export const contextController = {
-  render,
+  observe
 };
