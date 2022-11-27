71c60b666915a79b915bd3bb8e5511514ccf2e2e
trsaints <thiago.dev2k3@gmail.com>
2022-11-26 22:09:54 -0300

    Reorganização na estrutura do banco de produtos

--- app/src/services/products-service.js
+++ app/src/services/products-service.js
@@ -14,13 +14,8 @@ const checkPreLoad = () => {
 
 const setStructure = (db, data) => {
   const productsObjStore = db.createObjectStore("products", {
-    keyPath: "category",
-  });
-
-  data.forEach((list) => {
-    productsObjStore.createIndex(list.category, list.category, {
-      unique: true,
-    });
+    keyPath: "id",
+    autoIncrement: true,
   });
 
   productsObjStore.transaction.addEventListener("complete", (evt) => {
@@ -29,7 +24,12 @@ const setStructure = (db, data) => {
       .objectStore("products");
 
     data.forEach((list) => {
-      categoryObjStore.add(list);
+      const { category } = list;
+
+      list[category].forEach((product) => {
+        product.category = category;
+        categoryObjStore.add(product);
+      });
     });
 
     console.log("Banco de dados configurado com sucesso");
@@ -75,30 +75,16 @@ const loadAll = (record) => {
   });
 };
 
-const loadIndex = (index) => {
-  return new Promise((resolve, reject) => {
-    const request = window.indexedDB.open("ag_products", 1);
+const loadCategory = async (category) => {
+  const data = await loadAll("products");
+  const list = data.filter(product => product.category === category)
 
-    request.addEventListener("success", (evt) => {
-      const { result } = evt.target;
-      const transaction = result.transaction("products");
-      const objStore = transaction.objectStore("products");
-      const data = objStore.get(index);
-
-      data.addEventListener("success", () => {
-        resolve(data.result);
-      });
-
-      data.addEventListener("error", () =>
-        reject(new Error("Não foi possível realizar a operação"))
-      );
-    });
-  });
+  return list;
 };
 
 export const productsService = {
   checkPreLoad,
   configure,
   loadAll,
-  loadIndex,
+  loadCategory,
 };
